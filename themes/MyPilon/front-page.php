ee54248c314cb8f9bfe5ecf103425fb662cfcd6e
denis.shediakov <luckyswd@gmail.com>
2023-09-03 11:17:14 +0300

    fix out-partners

--- themes/MyPilon/front-page.php
+++ themes/MyPilon/front-page.php
@@ -9,13 +9,13 @@ include 'templates/led-screen-type.php';
 include 'templates/why-me.php';
 include 'templates/projects-slider.php';
 include 'templates/screen-placement.php';
-include 'templates/our-partners.php';
 include 'templates/solution-slider.php';
 include 'templates/delivery-methods.php';
 include 'templates/four-stages.php';
 include 'templates/about-company.php';
 include 'templates/product-quality.php';
 
+include 'templates/our-partners.php';
 include 'templates/contact-form.php';
 include 'templates/contact-form_two.php';
 include 'templates/contact-form_three.php';
--- themes/MyPilon/templates/our-partners.php
+++ themes/MyPilon/templates/our-partners.php
@@ -5,33 +5,19 @@ $our_partners_gallery = get_field('our_partners_gallery');
 
 <section id="our_partners">
     <div class="container">
-        <?php if (!empty($our_partners_title)): ?>
-        <h2 class="our_partners__title">
-            <?= $our_partners_title ?? '' ?>
-        </h2>
-        <?php endif; ?>
-        <?php if (!empty($our_partners_gallery)) : ?>
-        <div class="swiper-container our-partner-slider">
-            <div class="swiper-wrapper">
-                <?php foreach ($our_partners_gallery as $image) : ?>
-                <div class="our_partners__gallery_image swiper-slide">
-                    <img src="<?= $image['url'] ?>" alt="">
+        <div class="our_partners__wrapper">
+            <?php if (!empty($our_partners_title)): ?>
+                <h2 class="our_partners__title">
+                    <?= $our_partners_title ?>
+                </h2>
+            <?php endif; ?>
+            <?php if (!empty($our_partners_gallery)) : ?>
+                <div class="our_partners-gallery">
+                    <?php foreach ($our_partners_gallery as $image) : ?>
+                        <img loading="lazy" src="<?= $image['url'] ?>" alt="image">
+                    <?php endforeach; ?>
                 </div>
-                <?php endforeach; ?>
-            </div>
-            <div class="swiper-button-next">
-                <svg width="10" height="17" viewBox="0 0 10 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M0.999999 1L9 8.5L1 16" stroke="#171631" stroke-width="2" stroke-linecap="round"
-                        stroke-linejoin="round" />
-                </svg>
-            </div>
-            <div class="swiper-button-prev">
-                <svg width="10" height="17" viewBox="0 0 10 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M9 16L1 8.5L9 1" stroke="#171631" stroke-width="2" stroke-linecap="round"
-                        stroke-linejoin="round" />
-                </svg>
-            </div>
+            <?php endif; ?>
         </div>
-        <?php endif; ?>
     </div>
 </section>
