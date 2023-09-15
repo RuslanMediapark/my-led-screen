7cbd78481360927c744874b3380eba9c2417f053
Ruslan Matusevich <ruslan.matusevich@mediapark.com>
2023-09-15 20:50:56 +0200

    finish outdoor

--- themes/MyPilon/page-outdoor.php
+++ themes/MyPilon/page-outdoor.php
@@ -2,16 +2,23 @@
 get_header();
 
 include 'templates/main-slider.php';
-include 'templates/contact-form.php';
 include 'templates/indoor-and-street/mediapolis-is.php';
+include 'templates/solution-slider.php';
+include 'templates/contact-form.php';
 include 'templates/indoor-and-street/display-device.php';
-include 'templates/indoor-and-street/where-to-put-the-screens.php';
+include 'templates/indoor-and-street/screens_size.php';
 include 'templates/indoor-and-street/calculator-form.php';
 include 'templates/indoor-and-street/types_of_screens.php';
-include 'templates/indoor-and-street/screens_size.php';
 include 'templates/indoor-and-street/pixel-step.php';
 include 'templates/projects-slider.php';
+include 'templates/why-me.php';
 include 'templates/contact-form_two.php';
-include 'templates/our-partners.php';
+include 'templates/how-work.php';
+include 'templates/delivery-methods.php';
+include 'templates/other-products.php';
+include 'templates/contact-form_question.php';
 
 get_footer();
+
+
+
