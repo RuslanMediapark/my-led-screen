a018c0a55329b11b3069476c69bb47a7f6070ac6
Denis <denis.shediakov@intexsoft.by>
2023-09-16 16:32:03 +0300

    fix

--- themes/MyPilon/templates/media-pylons/main-module.php
+++ themes/MyPilon/templates/media-pylons/main-module.php
@@ -2,43 +2,54 @@
 $main_module_title = get_field('main_module_title');
 $main_module_subtitle = get_field('main_module_subtitle');
 $main_module_dignity = get_field('main_module_dignity');
+$main_module_mode_page = get_field('main_module_mode_page');
+$main_module_dignity_title = get_field('main_module_dignity_title');
+$main_module_image = get_field('main_module_image');
 ?>
 
 <section id="main-module">
     <div class="container">
         <div class="main-module__block">
-              <?php if (!empty($main_module_title)) : ?>
-                  <h1 class="main-module__block-left-title"><?= $main_module_title ?? '' ?></h1>
-              <?php endif; ?>
-              <?php if (!empty($main_module_subtitle)) : ?>
-                  <h2 class="main-module__block-left-subtitle"><?= $main_module_subtitle ?? '' ?></h2>
-              <?php endif; ?>
-              <?php if (!empty($main_module_dignity)) : ?>
-                  <div class="main-module__block-left-dignity">
+            <?php if (!empty($main_module_title)) : ?>
+                <h1 class="main-module__block-left-title"><?= $main_module_title ?? '' ?></h1>
+            <?php endif; ?>
+            <?php if (!empty($main_module_subtitle)) : ?>
+                <h2 class="main-module__block-left-subtitle"><?= $main_module_subtitle ?? '' ?></h2>
+            <?php endif; ?>
+            <?php if (!empty($main_module_dignity_title)) : ?>
+                <p class="dignity_title"><?= $main_module_dignity_title ?></p>
+            <?php endif; ?>
+            <?php if (!empty($main_module_dignity)) : ?>
+                <div class="main-module__block-left-dignity">
                     <?php foreach ($main_module_dignity as $item) : ?>
-                      <div class="main-module__block-item">
-                      <?php if (!empty($item['image'])) : ?>
-                            <div class="main-module__block-left-dignity-image">
-                                <img class="style-svg" src="<?= $item['image']['url'] ?>" alt="">
-                            </div>
-                      <?php endif; ?>
-                      <?php if (!empty($item['title'])) : ?>
-                            <div class="main-module__block-left-dignity-title">
-                                <p><?= $item['title'] ?? '' ?></p>
-                                <p><?= $item['description'] ?? ''?></p>
-                            </div>
-                      <?php endif; ?>
-                      </div>
+                        <div class="main-module__block-item">
+                            <?php if (!empty($item['image'])) : ?>
+                                <div class="main-module__block-left-dignity-image">
+                                    <img class="style-svg" src="<?= $item['image']['url'] ?>" alt="">
+                                </div>
+                            <?php endif; ?>
+                            <?php if (!empty($item['title'])) : ?>
+                                <div class="main-module__block-left-dignity-title">
+                                    <p><?= $item['title'] ?? '' ?></p>
+                                    <p><?= $item['description'] ?? '' ?></p>
+                                </div>
+                            <?php endif; ?>
+                        </div>
                     <?php endforeach; ?>
-                  </div>
-              <?php endif; ?>
-              <button class="btn" id="how-work-callback" data-fancybox="dialog" data-src="#dialog-content">
-                    Оставить заявку
-                </button>
-              <div class="main-module__block-right">
-                <div class="main-module__block-right-images">
                 </div>
-            </div>          
+            <?php endif; ?>
+            <button class="btn" id="how-work-callback" data-fancybox="dialog" data-src="#dialog-content">
+                Оставить заявку
+            </button>
+            <div class="main-module__block-right">
+                <?php if ($main_module_mode_page === 'media-pilons') : ?>
+                    <div class="main-module__block-right-images"></div>
+                <?php else: ?>
+                    <div class="block-right_image">
+                        <img loading="lazy" src="<?= $main_module_image['url'] ?? '' ?>" alt="image">
+                    </div>
+                <?php endif; ?>
+            </div>
         </div>
     </div>
 </section>
